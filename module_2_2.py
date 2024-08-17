1c08abf38914b68a0ef9d7395468ec152a3d616a
GorkaTsybin <egortsybin@yandex.ru>
2024-08-18 01:17:34 +0400

    Add files via upload

---
+++ module_2_2.py
@@ -0,0 +1,12 @@
+first = int(input('Введите первое число: '))
+second = int(input('Введите второе число: '))
+thirt = int(input('Введите третье число: '))
+
+if first == second == thirt:
+    print(3)
+
+elif first == thirt != second:
+    print(2)
+
+elif first != thirt != second:
+    print(0)
